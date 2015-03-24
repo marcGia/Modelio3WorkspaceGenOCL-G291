3abfebebada8cccd7a114d72ab7c978daf33aead
marc <marc.giaccone@laposte.net>
2015-03-24 08:44:09 +0100

    Ajout fichier pour projet SQLScribe

---
+++ macros/revSQL.py
@@ -0,0 +1,22 @@
+"""
+=========================================================
+                       GenOCL.py
+ Generate a USE OCL specification from a UML package
+=========================================================
+
+@author Giaccone Marc <marc.giaccone@laposte.net>
+@author Nawaoui Swane <swane.nawaoui@gmail.com>
+@group  G291
+
+Current state of the generator
+----------------------------------
+
+
+Current state of the tests
+--------------------------
+
+
+Observations
+------------
+Additional observations could go there
+"""
